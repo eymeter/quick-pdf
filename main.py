04cc8048f5b788a371f36fbd46e3255074ae3fc0
meymen <137542830+eymeter@users.noreply.github.com>
2023-08-12 23:06:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+
+key = "sec_MCrZJrSCdUjCqwIRFEjX8nvrO1wJGoXi"
+site = "https://api.chatpdf.com/v1"
+pdf = "https://dev-eym.me/summarize.pdf"
+started = True
+
+def chat():
+  headers = {"x-api-key": api_key}
+  response = requests.post(f"{site}/sources/add-url",
+                                   headers=headers,
+                                   json={"url": pdf})
+  response_data = response.json()
+  source_id = response_data["sourceId"]
+
+  while started:
+    user = input("+ ")
+    chat_response = requests.post(f"{base_url}/chats/message",
+                                  headers=headers,
+                                  json={
+                                    "sourceId":
+                                    source_id,
+                                    "messages": [{
+                                      "role": "user",
+                                      "content": user
+                                    }]
+                                  })
+    chat_data = chat_response.json()
+    print(f"- {chat_data}")
+    
+ 
+  
+
+
+
